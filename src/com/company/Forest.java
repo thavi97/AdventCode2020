80c2f2019c607c3ccabfe56eebf1a0c920e4f175
Thavi <thavit97@gmail.com>
2020-12-03 17:21:39 +0000

    Day 3 Task 1 Complete

--- src/com/company/Forest.java
+++ src/com/company/Forest.java
@@ -6,32 +6,75 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Forest {
+    
+    private static int numberOfRows;
+    private static int numberOfColumns;
+    private static int numberOfRepeats;
+    private static final int right = 3;
+    private static final int down = 1;
 
     public static void main(String[] args) throws FileNotFoundException {
         //System.out.println(Arrays.deepToString(readFile()));
-        readFile();
+        seekThroughMap(duplicateForest(readFile()));
+    }
+
+    public static void seekThroughMap(int[][] duplicateMapOfForest){
+        int x=0;
+        int y=0;
+        int counter = 0;
+        System.out.println(numberOfRows);
+        System.out.println((numberOfColumns*numberOfRepeats)+1);
+        while(y<numberOfRows-1){
+            x += right;
+            y += down;
+            if(duplicateMapOfForest[y][x] == 1 ){
+                counter++;
+                System.out.println(counter);
+            }
+        }
+    }
+
+    public static int[][] duplicateForest(int[][] mapOfForest) throws FileNotFoundException {
+        Scanner forest = new Scanner(new File("forest.txt"));
+        numberOfRepeats = ((numberOfRows/numberOfColumns)*(right+1));
+        int[][] duplicateMapOfForest = new int[numberOfRows][numberOfColumns*numberOfRepeats];
+        for (int j = 0; j < numberOfRows; j++) {
+            int u=0;
+            if (forest.hasNextLine()) {
+                char[] nextLine = forest.nextLine().toCharArray();
+                for(int i=0; i<numberOfRepeats; i++){
+                    for (char location : nextLine) {
+                        if(location == ".".charAt(0)){
+                            duplicateMapOfForest[j][u] = 0;
+                        }else{
+                            duplicateMapOfForest[j][u] = 1;
+                        }
+                        u++;
+                        //System.out.print(location);
+                    }
+                }
+                //System.out.println();
+            }
+            //System.out.print(Arrays.deepToString(duplicateMapOfForest));
+        }
+        return duplicateMapOfForest;
     }
 
     public static int[][] readFile() throws FileNotFoundException {
         Scanner forest = new Scanner(new File("forest.txt"));
         Scanner forestCheck = new Scanner(new File("forest.txt"));
         String currentLine = forestCheck.nextLine();
-        int numberOfColumns = currentLine.length();
+        numberOfColumns = currentLine.length();
 
         int i=1;
         while(forestCheck.hasNext()){
             forestCheck.nextLine();
             i++;
         }
-        int numberOfRows = i;
-
-        System.out.println(numberOfColumns);
-        System.out.println(numberOfRows);
-        System.out.println(numberOfRows/numberOfColumns);
+        numberOfRows = i;
 
         int[][] mapOfForest = new int[numberOfRows][numberOfColumns];
 
-        System.out.println("test");
         for (int j = 0; j < numberOfRows; j++) {
             int u=0;
             if (forest.hasNextLine()) {
@@ -52,7 +95,7 @@ public class Forest {
             forest.nextLine();
         }
 
-        System.out.print(Arrays.deepToString(mapOfForest));
+        //System.out.print(Arrays.deepToString(mapOfForest));
 
         forest.close();
         forestCheck.close();
